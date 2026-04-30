717bc16a5774b1e73350d353f6fbe6126861fc66
Ronny Sanchez <sronny@gmail.com>
2026-04-30 12:57:26 -0500

    Api2

--- encuestas/urls.py
+++ encuestas/urls.py
@@ -16,6 +16,6 @@ urlpatterns = [
     path('simulador/', views.simulador_pantallas, name='simulador_pantallas'),
     path('ejecutar-sorteo-ajax/', views.ejecutar_sorteo_ajax, name='ejecutar_sorteo_ajax'),
     path('api/<int:tienda_id>/', views.consultaEncuestaFijaTiendaApi, name='consultaApi'),
-    path('encuesta-completada/', views.encuesta_completada, name='encuesta_completada'),
+#    path('encuesta-completada/', views.encuesta_completada, name='encuesta_completada'),
 ]
 
